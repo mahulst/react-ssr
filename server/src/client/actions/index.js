0a2213ff2dc32dcb2e3c8ba3538865861a853a8c
Michel van der Hulst <michel@voorkanter.com>
2018-01-02 20:57:31 +0100

    feat: add login & logout functionality

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -1,4 +1,5 @@
 export const FETCH_USERS = 'fetch users';
+export const FETCH_CURRENT_USERS = 'fetch current user';
 
 export const fetchUsers = () => async (dispatch, getState, api) => {
   const res = await api.get('/users');
@@ -7,4 +8,12 @@ export const fetchUsers = () => async (dispatch, getState, api) => {
     type: FETCH_USERS,
     payload: res
   });
-}
+};
+
+export const fetchCurrentUser = () => async (dispatch, getState, api) => {
+  const res = await api.get('/current_user');
+  dispatch({
+    type: FETCH_CURRENT_USERS,
+    payload: res
+  });
+};
